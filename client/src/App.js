f5583e92ff03628376c5d742d43083852d394819
Andrej Stojkovski <andrej.stojkovski@students.finki.ukim.mk>
2026-05-16 21:13:10 +0200

    update

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>NVD Task Tracker - Frontend</h1>
+                <p>Добредојдовте во системот за следење задачи.</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
